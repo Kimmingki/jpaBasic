91fa76fb18d112e5be8a414ca81b5e99dcce72a7
Kimmingki <rlaalsrl9569@naver.com>
2024-07-15 19:12:08 +0900

    JPA - 애플리케이션 개발

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -5,12 +5,30 @@ import jakarta.persistence.*;
 public class JpaMain {
 
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
+        EntityTransaction tx = em.getTransaction();
 
-        em.close();
+        // code
+        tx.begin();
+
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+            findMember.setName("HelloJPA");
+
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("Hello");
+//            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
