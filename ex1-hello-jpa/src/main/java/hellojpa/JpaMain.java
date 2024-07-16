753dddb27454302dd47c4de0a912563fa9d26124
Kimmingki <rlaalsrl9569@naver.com>
2024-07-16 22:12:10 +0900

    영속성 관리 - 내부 동작 방식

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,15 +13,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
+            // 비영속
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(151L, "B");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("=== AFTER ===");
+
+            Member findMember = em.find(Member.class, 101L);
+
             System.out.println("findMember.id = " + findMember.getId());
             System.out.println("findMember.name = " + findMember.getName());
-            findMember.setName("HelloJPA");
 
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("Hello");
-//            em.persist(member);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
