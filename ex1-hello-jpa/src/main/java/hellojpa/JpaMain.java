9639f76480da9036d69ae1f9e4a20f49e4d7ff5c
Kimmingki <rlaalsrl9569@naver.com>
2024-07-17 22:24:51 +0900

    필드와 컬럼 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,21 +13,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(151L, "B");
-
-            // 영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("=== AFTER ===");
-
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
