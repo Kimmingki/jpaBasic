54dada278eac487f6792416453f49d9f2f0a2a02
Kimmingki <rlaalsrl9569@naver.com>
2024-07-25 21:35:21 +0900

    실전 예제 3 - 다양한 연관관계 매핑

--- jpabook/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpabook/src/main/java/jpabook/jpashop/domain/Order.java
@@ -24,6 +24,10 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 
