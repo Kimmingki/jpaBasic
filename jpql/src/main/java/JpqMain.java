98a4a27a1d9912de04841d31884c4e5dc94c7357
Kimmingki <rlaalsrl9569@naver.com>
2024-07-31 22:49:07 +0900

    JPQL 함수

--- jpql/src/main/java/JpqMain.java
+++ jpql/src/main/java/JpqMain.java
@@ -31,13 +31,7 @@ public class JpqMain {
             em.flush();
             em.clear();
 
-            String query =
-                    "select " +
-                            "case when m.age <= 10 then '학생요금' " +
-                            "     when m.age >= 60 then '경로요금' " +
-                            "     else '기본요금' " +
-                            "end " +
-                            "from Member m";
+            String query = "select concat('a', 'b') from Member m";
             List<String> result = em.createQuery(query, String.class).getResultList();
             for (String s : result) {
                 System.out.println("s = " + s);
