6c22f78835f822cca29e7957a0cce237aa0447bf
Kimmingki <rlaalsrl9569@naver.com>
2024-07-31 21:23:34 +0900

    프로젝션(select)

--- jpql/src/main/java/JpqMain.java
+++ jpql/src/main/java/JpqMain.java
@@ -1,5 +1,6 @@
 import jakarta.persistence.*;
 import jpql.Member;
+import jpql.MemberDTO;
 
 import java.util.List;
 
@@ -19,20 +20,17 @@ public class JpqMain {
             member.setAge(10);
             em.persist(member);
 
-//            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-//            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-//            Query query3 = em.createQuery("select m.username, m.age from Member m");
+//            List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                    .getResultList();
+//            Member findMember = result.get(0);
+//            findMember.setAge(20);
 
-//            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-//            List<Member> resultList = query.getResultList();
-//            for (Member member1 : resultList) {
-//                System.out.println("member1 = " + member1);
-//            }
-
-            Member singleResult = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("singleResult.getUsername() = " + singleResult.getUsername());
+            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+            for (MemberDTO memberDTO : resultList) {
+                System.out.println("memberDTO = " + memberDTO.getUsername());
+                System.out.println("memberDTO = " + memberDTO.getAge());
+            }
 
             tx.commit();
         } catch (Exception e) {
